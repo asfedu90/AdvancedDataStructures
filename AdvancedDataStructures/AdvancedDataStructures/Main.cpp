27ea9dd9efe582ac870748bb51da7956245912aa
asfedu90 <32417482+asfedu90@users.noreply.github.com>
2017-09-30 12:46:25 -0400

    Merging From Ajoy's Computer

--- AdvancedDataStructures/AdvancedDataStructures/Main.cpp
+++ AdvancedDataStructures/AdvancedDataStructures/Main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
 
-	cout << " Hello World by Ajoy and Jennifer"<< endl;
-	cout << "Jennifer's Part";
+	cout << "Hello World by Ajoy and Jennifer"<< endl;
+	cout << "Jennifer's Part" <<endl;
 	cout << "Ajoy's Part";
 
 	return 0;
